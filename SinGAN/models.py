87c9768f46e34f4834d2f6c0d4432cc7f24e6127
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2021-03-07 01:05:23 +0200

    adding attention to the model

--- SinGAN/models.py
+++ SinGAN/models.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 import numpy as np
 import torch.nn.functional as F
 
+from torch.autograd import Variable
+import numpy as np
+
 
 class ConvBlock(nn.Sequential):
     def __init__(self, in_channel, out_channel, ker_size, padd, stride):
@@ -61,3 +64,109 @@ class GeneratorConcatSkip2CleanAdd(nn.Module):
         ind = int((y.shape[2]-x.shape[2])/2)
         y = y[:,:,ind:(y.shape[2]-ind),ind:(y.shape[3]-ind)]
         return x+y
+
+
+class Self_Attn(nn.Module):
+    """ Self attention Layer"""
+
+    def __init__(self, in_dim):
+        super().__init__()
+
+        # Construct the conv layers
+        self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 2, kernel_size=1)
+        self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 2, kernel_size=1)
+        self.value_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+
+        # Initialize gamma as 0
+        self.gamma = nn.Parameter(torch.zeros(1))
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        """
+            inputs :
+                x : input feature maps( B * C * W * H)
+            returns :
+                out : self attention value + input feature
+                attention: B * N * N (N is Width*Height)
+        """
+        m_batchsize, C, width, height = x.size()
+
+        proj_query = self.query_conv(x).view(m_batchsize, -1, width * height).permute(0, 2, 1)  # B * N * C
+        proj_key = self.key_conv(x).view(m_batchsize, -1, width * height)  # B * C * N
+        energy = torch.bmm(proj_query, proj_key)  # batch matrix-matrix product
+
+        attention = self.softmax(energy)  # B * N * N
+        proj_value = self.value_conv(x).view(m_batchsize, -1, width * height)  # B * C * N
+        out = torch.bmm(proj_value, attention.permute(0, 2, 1))  # batch matrix-matrix product
+        out = out.view(m_batchsize, C, width, height)  # B * C * W * H
+
+        # Add attention weights onto input
+        out = self.gamma * out + x
+        return out, attention
+
+
+class MyConvBlock(nn.Sequential):
+    def __init__(self, in_channel, out_channel, ker_size, padd, stride):
+        super(MyConvBlock, self).__init__()
+        self.add_module('conv', nn.Conv2d(in_channel, out_channel, kernel_size=ker_size, stride=stride, padding=padd)),
+        self.add_module('norm', nn.BatchNorm2d(out_channel)),
+        self.add_module('LeakyRelu', nn.LeakyReLU(0.2, inplace=True))
+
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv2d') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('Norm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+
+class MyWDiscriminator(nn.Module):
+    def __init__(self, opt):
+        super(MyWDiscriminator, self).__init__()
+        self.is_cuda = torch.cuda.is_available()
+        N = int(opt.nfc)
+        self.head = ConvBlock(opt.nc_im, N, opt.ker_size, opt.padd_size, 1)
+        self.body = nn.Sequential()
+        for i in range(opt.num_layer - 2):
+            N = int(opt.nfc / pow(2, (i + 1)))
+            block = ConvBlock(max(2 * N, opt.min_nfc), max(N, opt.min_nfc), opt.ker_size, opt.padd_size, 1)
+            self.body.add_module('block%d' % (i + 1), block)
+        self.attn = Self_Attn( max(N, opt.min_nfc))
+        self.tail = nn.Conv2d(max(N, opt.min_nfc), 1, kernel_size=opt.ker_size, stride=1, padding=opt.padd_size)
+
+    def forward(self, x):
+        x = self.head(x)
+        x = self.body(x)
+        x,_ = self.attn(x)
+        x = self.tail(x)
+        return x
+
+
+class MyGeneratorConcatSkip2CleanAdd(nn.Module):
+    def __init__(self, opt):
+        super(MyGeneratorConcatSkip2CleanAdd, self).__init__()
+        self.is_cuda = torch.cuda.is_available()
+        N = opt.nfc
+        self.head = ConvBlock(opt.nc_im, N, opt.ker_size, opt.padd_size,
+                              1)  # GenConvTransBlock(opt.nc_z,N,opt.ker_size,opt.padd_size,opt.stride)
+        self.body = nn.Sequential()
+        for i in range(opt.num_layer - 2):
+            N = int(opt.nfc / pow(2, (i + 1)))
+            block = ConvBlock(max(2 * N, opt.min_nfc), max(N, opt.min_nfc), opt.ker_size, opt.padd_size, 1)
+            self.body.add_module('block%d' % (i + 1), block)
+        self.attn = Self_Attn(max(N, opt.min_nfc))
+        self.tail = nn.Sequential(
+            nn.Conv2d(max(N, opt.min_nfc), opt.nc_im, kernel_size=opt.ker_size, stride=1, padding=opt.padd_size),
+            nn.Tanh()
+        )
+
+    def forward(self, x, y):
+        x = self.head(x)
+        x = self.body(x)
+        x,_ = self.attn(x)
+        x = self.tail(x)
+        ind = int((y.shape[2] - x.shape[2]) / 2)
+        y = y[:, :, ind:(y.shape[2] - ind), ind:(y.shape[3] - ind)]
+        return x + y
