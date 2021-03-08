f27931d5c5996ace3b7123546eb348358e19eaba
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2021-03-08 19:42:42 +0200

    Update functions.py

--- SinGAN/functions.py
+++ SinGAN/functions.py
@@ -127,24 +127,25 @@ def move_to_cpu(t):
 
 def calc_gradient_penalty(netD, real_data, fake_data, LAMBDA, device):
     #print real_data.size()
-    alpha = torch.rand(1, 1)
-    alpha = alpha.expand(real_data.size())
-    alpha = alpha.to(device)#cuda() #gpu) #if use_cuda else alpha
+    with torch.enable_grad():
+        alpha = torch.rand(1, 1)
+        alpha = alpha.expand(real_data.size())
+        alpha = alpha.to(device)#cuda() #gpu) #if use_cuda else alpha
 
-    interpolates = alpha * real_data + ((1 - alpha) * fake_data)
+        interpolates = alpha * real_data + ((1 - alpha) * fake_data)
 
 
-    interpolates = interpolates.to(device)#.cuda()
-    interpolates = torch.autograd.Variable(interpolates, requires_grad=True)
+        interpolates = interpolates.to(device)#.cuda()
+        interpolates = torch.autograd.Variable(interpolates, requires_grad=True)
 
-    disc_interpolates = netD(interpolates)
+        disc_interpolates = netD(interpolates)
 
-    gradients = torch.autograd.grad(outputs=disc_interpolates, inputs=interpolates,
-                              grad_outputs=torch.ones(disc_interpolates.size()).to(device),#.cuda(), #if use_cuda else torch.ones(
-                                  #disc_interpolates.size()),
-                              create_graph=True, retain_graph=True, only_inputs=True)[0]
-    #LAMBDA = 1
-    gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean() * LAMBDA
+        gradients = torch.autograd.grad(outputs=disc_interpolates, inputs=interpolates,
+                                  grad_outputs=torch.ones(disc_interpolates.size()).to(device),#.cuda(), #if use_cuda else torch.ones(
+                                      #disc_interpolates.size()),
+                                  create_graph=True, retain_graph=True, only_inputs=True)[0]
+        #LAMBDA = 1
+        gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean() * LAMBDA
     return gradient_penalty
 
 def read_image(opt):
