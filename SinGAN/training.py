97bf10bd8b5a860e6de7fb71d860a831c80081eb
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2021-03-08 21:00:35 +0200

    Update training.py

--- SinGAN/training.py
+++ SinGAN/training.py
@@ -180,7 +180,7 @@ def train_single_scale(netD,netG,reals,Gs,Zs,in_s,NoiseAmp,opt,centers=None):
             loss = functions.GradientPaneltyLoss(netD, opt.lambda_grad, opt.device)
             #gradient_penalty = functions.calc_gradient_penalty(netD, real, fake, opt.lambda_grad, opt.device)
             gradient_penalty = checkpoint(loss, real, fake)
-            gradient_penalty.backward()
+            loss.backward()
 
             errD = errD_real + errD_fake + gradient_penalty
             optimizerD.step()
