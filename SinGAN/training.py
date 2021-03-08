04ce2913ce9fbe58d85fb9bc3506939aeccb03ea
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2021-03-08 20:21:23 +0200

    .

--- SinGAN/training.py
+++ SinGAN/training.py
@@ -178,11 +178,11 @@ def train_single_scale(netD,netG,reals,Gs,Zs,in_s,NoiseAmp,opt,centers=None):
             D_G_z = output.mean().item()
 
             gradient_penalty = functions.calc_gradient_penalty(netD, real, fake, opt.lambda_grad, opt.device)
+            
             gradient_penalty.backward()
 
             errD = errD_real + errD_fake + gradient_penalty
             optimizerD.step()
-            del gradient_penalty
 
         errD2plot.append(errD.detach())
 
