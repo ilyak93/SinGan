b4bb1ce32fbf1657c1a05a5419f01b4a933a141e
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2021-03-08 21:26:02 +0200

    .

--- SinGAN/training.py
+++ SinGAN/training.py
@@ -129,7 +129,7 @@ def train_single_scale(netD,netG,reals,Gs,Zs,in_s,NoiseAmp,opt,centers=None):
             #output = checkpoint(netD, real)
             #D_real_map = output.detach()
             errD_real = -output.mean()#-a
-            #errD_real.backward(retain_graph=True)
+            errD_real.backward(retain_graph=True)
             D_x = -errD_real.item()
 
             # train with fake
@@ -174,14 +174,13 @@ def train_single_scale(netD,netG,reals,Gs,Zs,in_s,NoiseAmp,opt,centers=None):
             output = netD(fake.detach())
             #output = checkpoint(netD, fake.detach())
             errD_fake = output.mean()
-            #errD_fake.backward(retain_graph=True)
+            errD_fake.backward(retain_graph=True)
             D_G_z = output.mean().item()
 
             gradient_penalty = functions.calc_gradient_penalty(netD, real, fake, opt.lambda_grad, opt.device)
-            #gradient_penalty.backward()
+            gradient_penalty.backward()
 
             errD = errD_real + errD_fake + gradient_penalty
-            errD.backward()
             optimizerD.step()
 
         errD2plot.append(errD.detach())
