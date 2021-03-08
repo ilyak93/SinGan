22a37be5c03819a6dd7b397e1289717d434c8ccd
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2021-03-08 21:04:42 +0200

    regular gradient penality

--- SinGAN/functions.py
+++ SinGAN/functions.py
@@ -15,7 +15,6 @@ import random
 from sklearn.cluster import KMeans
 
 
-
 # custom weights initialization called on netG and netD
 
 def read_image(opt):
@@ -124,37 +123,6 @@ def move_to_gpu(t):
 def move_to_cpu(t):
     t = t.to(torch.device('cpu'))
     return t
-
-class GradientPaneltyLoss(nn.Module):
-    def __init__(self, netD, LAMBDA, device):
-        super(GradientPaneltyLoss, self).__init__()
-        self.netD = netD
-        self.LAMBDA = LAMBDA
-        self.device = device
-
-    def forward(self, real_data, fake_data):
-        """Compute gradient penalty: (L2_norm(dy/dx) - 1)**2."""
-        #print real_data.size()
-        with torch.enable_grad():
-            alpha = torch.rand(1, 1)
-            alpha = alpha.expand(real_data.size())
-            alpha = alpha.to(self.device)#cuda() #gpu) #if use_cuda else alpha
-
-            interpolates = alpha * real_data + ((1 - alpha) * fake_data)
-
-
-            interpolates = interpolates.to(self.device)#.cuda()
-            interpolates = torch.autograd.Variable(interpolates, requires_grad=True)
-
-            disc_interpolates = self.netD(interpolates)
-
-            gradients = torch.autograd.grad(outputs=disc_interpolates, inputs=interpolates,
-                                      grad_outputs=torch.ones(disc_interpolates.size()).to(self.device),#.cuda(), #if use_cuda else torch.ones(
-                                          #disc_interpolates.size()),
-                                      create_graph=True, retain_graph=True, only_inputs=True)[0]
-            #LAMBDA = 1
-            gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean() * self.LAMBDA
-            return gradient_penalty
 
 def calc_gradient_penalty(netD, real_data, fake_data, LAMBDA, device):
     #print real_data.size()
--- SinGAN/training.py
+++ SinGAN/training.py
@@ -176,11 +176,9 @@ def train_single_scale(netD,netG,reals,Gs,Zs,in_s,NoiseAmp,opt,centers=None):
             errD_fake = output.mean()
             errD_fake.backward(retain_graph=True)
             D_G_z = output.mean().item()
-            
-            loss = functions.GradientPaneltyLoss(netD, opt.lambda_grad, opt.device)
-            #gradient_penalty = functions.calc_gradient_penalty(netD, real, fake, opt.lambda_grad, opt.device)
-            gradient_penalty = checkpoint(loss, real, fake)
-            loss.backward()
+
+            gradient_penalty = functions.calc_gradient_penalty(netD, real, fake, opt.lambda_grad, opt.device)
+            gradient_penalty.backward()
 
             errD = errD_real + errD_fake + gradient_penalty
             optimizerD.step()
