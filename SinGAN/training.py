840f39099c1851e8aa945898a8c54ad12f05b453
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2021-03-08 19:38:59 +0200

    Update training.py

--- SinGAN/training.py
+++ SinGAN/training.py
@@ -169,25 +169,17 @@ def train_single_scale(netD,netG,reals,Gs,Zs,in_s,NoiseAmp,opt,centers=None):
             else:
                 noise = opt.noise_amp*noise_+prev
 
-            #fake = netG(noise.detach(),prev)
-            #fake = checkpoint(netG, noise.detach(),prev)
-            # set the number of checkpoint segments
-            segments = 2
-
-            # get the modules in the model. These modules should be in the order
-            # the model should be executed
-            modules = [module for k, module in netG._modules.items()]
-
-            # now call the checkpoint API and get the output
-            fake = checkpoint_sequential(modules, segments, noise.detach())
-            
+            fake = netG(noise.detach(),prev)
+         
             output = netD(fake.detach())
             #output = checkpoint(netD, fake.detach())
             errD_fake = output.mean()
             errD_fake.backward(retain_graph=True)
             D_G_z = output.mean().item()
 
-            gradient_penalty = functions.calc_gradient_penalty(netD, real, fake, opt.lambda_grad, opt.device)
+            #gradient_penalty = functions.calc_gradient_penalty(netD, real, fake, opt.lambda_grad, opt.device)
+			gradient_penalty = checkpoint(functions.calc_gradient_penalty, netD, real, fake, opt.lambda_grad, opt.device)
+			
             gradient_penalty.backward()
 
             errD = errD_real + errD_fake + gradient_penalty
