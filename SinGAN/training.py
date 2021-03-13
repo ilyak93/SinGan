3efc51481217a94f0e53f85a956988deb7ee65e8
ilyak93 <ilyak93@gmail.com>
2021-03-13 20:55:40 +0200

    Update training.py

--- SinGAN/training.py
+++ SinGAN/training.py
@@ -10,7 +10,6 @@ from SinGAN.imresize import imresize
 from torch.utils.checkpoint import checkpoint
 from torch.utils.checkpoint import checkpoint_sequential
 #from memory_saving_gradients import gradients
-%matplotlib inline
 
 def train(opt,Gs,Zs,reals,NoiseAmp):
     real_ = functions.read_image(opt)
