cf7fe22ae354568632bb24d95588f5ccbcd0e127
ilyak93 <ilyak93@gmail.com>
2021-03-15 00:41:01 +0200

    attention experiment

--- SinGAN/models.py
+++ SinGAN/models.py
@@ -401,3 +401,85 @@ class My22GeneratorConcatSkip2CleanAdd(nn.Module):
         ind = int((y.shape[2] - x.shape[2]) / 2)
         y = y[:, :, ind:(y.shape[2] - ind), ind:(y.shape[3] - ind)]
         return x + y
+
+        
+
+class My23WDiscriminator(nn.Module):
+    def __init__(self, opt):
+        super(My23WDiscriminator, self).__init__()
+        self.is_cuda = torch.cuda.is_available()
+        N = int(opt.nfc)
+        self.head = ConvBlock(opt.nc_im, N, opt.ker_size, opt.padd_size, 1)
+        self.body = nn.Sequential()
+        for i in range(opt.num_layer - 2):
+            N = int(opt.nfc / pow(2, (i + 1)))
+            block = ConvBlock(max(2 * N, opt.min_nfc), max(N, opt.min_nfc), opt.ker_size, opt.padd_size, 1)
+            self.body.add_module('block%d' % (i + 1), block)
+        if opt.attn == True:
+            self.attn = AxialAttention(
+                dim=max(N, opt.min_nfc),  # embedding dimension
+                dim_index=1,  # where is the embedding dimension
+                # dim_heads = 32,        # dimension of each head. defaults to dim // heads if not supplied
+                heads=4,  # number of heads for multi-head attention
+                num_dimensions=2,  # number of axial dimensions (images is 2, video is 3, or more)
+                sum_axial_out=True
+                # whether to sum the contributions of attention on each axis, or to run the input through them sequentially. defaults to true
+            )
+            self.ffn = nn.Sequential(nn.Linear(max(N, opt.min_nfc), max(2 * N, opt.min_nfc), bias=True),
+                                     nn.ReLU(),
+                                     nn.Linear(max(2 * N, opt.min_nfc), max(N, opt.min_nfc), bias=True))
+        self.tail = nn.Conv2d(max(N, opt.min_nfc), 1, kernel_size=opt.ker_size, stride=1, padding=opt.padd_size)
+
+    def forward(self, x):
+        x = self.head(x)
+        x = self.body(x)
+        if hasattr(self, 'attn'):
+            x = x + self.attn(x)
+            x = x.permute([0, 2, 3, 1])
+            x = self.ffn(x).permute([0, 3, 1, 2])
+        x = self.tail(x)
+        return x
+
+
+class My23GeneratorConcatSkip2CleanAdd(nn.Module):
+    def __init__(self, opt):
+        super(My23GeneratorConcatSkip2CleanAdd, self).__init__()
+        self.is_cuda = torch.cuda.is_available()
+        N = opt.nfc
+        self.head = ConvBlock(opt.nc_im, N, opt.ker_size, opt.padd_size,
+                              1)  # GenConvTransBlock(opt.nc_z,N,opt.ker_size,opt.padd_size,opt.stride)
+        self.body = nn.Sequential()
+        for i in range(opt.num_layer - 2):
+            N = int(opt.nfc / pow(2, (i + 1)))
+            block = ConvBlock(max(2 * N, opt.min_nfc), max(N, opt.min_nfc), opt.ker_size, opt.padd_size, 1)
+            self.body.add_module('block%d' % (i + 1), block)
+        if opt.attn == True:
+            self.attn = self.attn = AxialAttention(
+                dim=max(N, opt.min_nfc),  # embedding dimension
+                dim_index=1,  # where is the embedding dimension
+                # dim_heads = 32,        # dimension of each head. defaults to dim // heads if not supplied
+                heads=4,  # number of heads for multi-head attention
+                num_dimensions=2,  # number of axial dimensions (images is 2, video is 3, or more)
+                sum_axial_out=True
+                # whether to sum the contributions of attention on each axis, or to run the input through them sequentially. defaults to true
+            )
+            self.ffn = nn.Sequential(nn.Linear(max(N, opt.min_nfc), max(2 * N, opt.min_nfc), bias=True),
+                                     nn.ReLU(),
+                                     nn.Linear(max(2 * N, opt.min_nfc), max(N, opt.min_nfc), bias=True))
+        self.tail = nn.Sequential(
+            nn.Conv2d(max(N, opt.min_nfc), opt.nc_im, kernel_size=opt.ker_size, stride=1, padding=opt.padd_size),
+            nn.Tanh()
+        )
+
+    def forward(self, x, y):
+        x = self.head(x)
+        x = self.body(x)
+        if hasattr(self, 'attn'):
+            # x,_ = self.attn(x)
+            x = x + self.attn(x)
+            x = x.permute([0,2,3,1])
+            x = self.ffn(x).permute([0,3,1,2])
+        x = self.tail(x)
+        ind = int((y.shape[2] - x.shape[2]) / 2)
+        y = y[:, :, ind:(y.shape[2] - ind), ind:(y.shape[3] - ind)]
+        return x + y
